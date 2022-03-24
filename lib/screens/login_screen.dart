a0f2f49c721e0ca7862c423c6a664a2af4b7ad1a
akbulutdora <akbulutdora@gmail.com>
2022-03-25 01:24:34 +0300

    initialize screens

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+// EGECAN
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/my_library_screen.dart
@@ -0,0 +1,12 @@
+// BENGİSU
+
+import 'package:flutter/material.dart';
+
+class MyLibraryScreen extends StatelessWidget {
+  const MyLibraryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
