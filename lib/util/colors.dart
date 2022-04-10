cef2e7d8e48ab9b4a3c74aa7d391a87f8829bb7a
akbulutdora <akbulutdora@gmail.com>
2022-04-10 21:39:15 +0300

    Update colors.dart

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primary = Colors.blue;
+  static Color primary = const Color(0xff4e89fd);
   static Color secondary = Colors.black;
+  static Color line = const Color(0xffEEF1F4);
+  static Color boxFrame = const Color(0xffDEDEDE);
+  static Color lightText = const Color(0xffc4c4c4);
+  static Color contentText = const Color(0xff949494);
+  static Color contentBoxFill = const Color(0xfff8fbff);
 
   // TODO: ADD OTHER COLORS
 
