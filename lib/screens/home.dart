fcd1ab4f31c505cbf5ca917a25d087ee466e3313
bengisu <bengisueren@sabanciuniv.edu>
2022-04-16 15:21:53 +0300

    ara commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -38,7 +38,7 @@ class _HomeState extends State<Home> {
     HomeScreen(),
     const MyExploreScreen(),
     const NewPostScreen(),
-    const MyLibraryScreen(),
+    MyLibraryScreen(),
     const SearchScreen(),
   ];
 
--- lib/screens/my_library_screen.dart
+++ lib/screens/my_library_screen.dart
@@ -4,46 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:upcarta_mobile_app/models/models.dart';
 import 'package:upcarta_mobile_app/components/content_list.dart';
 
-/*
-class MyLibraryScreen extends StatelessWidget {
-  static MaterialPage page() {
-    return MaterialPage(
-      name: UpcartaPages.loginPath,
-      key: ValueKey(UpcartaPages.loginPath),
-      child: const MyLibraryScreen(),
-    );
-  }
 
-  const MyLibraryScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
-  }
-}*/
-
-void main() => runApp(const MyLibraryScreen());
+//void main() => runApp(const MyLibraryScreen());
 
 class MyLibraryScreen extends StatelessWidget {
   static MaterialPage page() {
     return MaterialPage(
       name: UpcartaPages.libraryPath,
       key: ValueKey(UpcartaPages.libraryPath),
-      child: const MyLibraryScreen(),
+      child: MyLibraryScreen(),
     );
   }
 
-  const MyLibraryScreen({Key? key}) : super(key: key);
-  static const String _title = 'Flutter Code Sample';
-
+  MyLibraryScreen({Key? key}) : super(key: key);
+  //static const String _title = 'Flutter Code Sample';
+/*
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
+    return MaterialApp(
+      //title: _title,
       home: MyStatelessWidget(),
     );
   }
-}
+}*/
 
 final List<Content> contents = <Content>[
   Content(
@@ -74,9 +57,11 @@ final List<Content> contents = <Content>[
       added_by_id: '',
       image: ''),
 ];
-
+/*
 class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+  MyStatelessWidget({Key? key}) : super(key: key);
+*/
+
 
   @override
   Widget build(BuildContext context) {
@@ -85,6 +70,7 @@ class MyStatelessWidget extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
+          toolbarHeight: 0,
           bottom: const TabBar(
             tabs: <Widget>[
               Tab(text: 'Saved' //, icon: Icon(Icons.bookmark_border_outlined),
@@ -96,6 +82,7 @@ class MyStatelessWidget extends StatelessWidget {
               Tab(text: 'Recos' //, icon: Icon(Icons.thumb_up_outlined),
                   ),
             ],
+            labelColor: Colors.black54,
           ),
         ),
         body: TabBarView(
@@ -108,17 +95,50 @@ class MyStatelessWidget extends StatelessWidget {
                         onPressed: () {
                           //TODO: IMPLEMENT BUTTON FUNCTIONALITY
                         },
-                        child: Text("Button1")),
+                        child: Text("Button1")
+                    ),
                   ],
                 ),
                 Row(
-                  children: <Widget>[
-                    Text("Row2"),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:[
+                    Row(children:[
+                      Text("Filter")
+                    ]),
+                    Row(children:[
+                      Icon(Icons.person),
+                      Icon(Icons.tag),
+                      Icon(Icons.format_list_bulleted_rounded)
+                    ]),
                   ],
                 ),
+                new Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: new Divider()
+                ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text("Row3"),
+                    Chip(
+                      avatar: Icon(Icons.format_list_bulleted_rounded),
+                      label: const Text('All'),
+                    ),
+                    Chip(
+                      avatar: Icon(Icons.menu_book),
+                      label: const Text('Book'),
+                    ),
+                    Chip(
+                      avatar: Icon(Icons.videocam_outlined),
+                      label: const Text('Video'),
+                    ),
+                    Chip(
+                      avatar: Icon(Icons.podcasts),
+                      label: const Text('Podcast'),
+                    ),
+                    Chip(
+                      avatar: Icon(Icons.newspaper),
+                      label: const Text('Art'),
+                    ),
                   ],
                 ),
                 ContentList(
@@ -142,88 +162,4 @@ class MyStatelessWidget extends StatelessWidget {
   }
 }
 
-/*
-            Center(
-              child: Text("It's cloudy here"),
-            ),
-            Center(
-              child: Text("It's rainy here"),
-            ),
-            Center(
-              child: Text("It's sunny here"),
-            ),
-            Center(
-              child: Text("It's hello here"),
-            ),
-            */
 
-var stars = Row(
-  mainAxisSize: MainAxisSize.min,
-  children: [
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-    Icon(Icons.star, color: Colors.green[500]),
-    const Icon(Icons.star, color: Colors.black),
-    const Icon(Icons.star, color: Colors.black),
-  ],
-);
-
-final ratings = Container(
-  padding: const EdgeInsets.all(20),
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      stars,
-      const Text(
-        '170 Reviews',
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w800,
-          fontFamily: 'Roboto',
-          letterSpacing: 0.5,
-          fontSize: 20,
-        ),
-      ),
-    ],
-  ),
-);
-
-////////////////
-
-class DropDownButton extends StatefulWidget {
-  const DropDownButton({Key? key}) : super(key: key);
-
-  @override
-  State<DropDownButton> createState() => _MyDropDownButtonState();
-}
-
-class _MyDropDownButtonState extends State<DropDownButton> {
-  String dropdownValue = 'One';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      borderRadius: BorderRadius.circular(2),
-      icon: const Icon(Icons.arrow_downward),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>[
-        'MOST RECOMMENDED',
-        'FOLLOWING',
-        'NEW TO OLD',
-        'OLD TO NEW',
-        'DEFAULT'
-      ].map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-    ;
-  }
-}
