5847f0339e7d08c8e36fe45707e00e9d8486be0d
brnyildiran <brnyildiran@gmail.com>
2022-05-19 01:30:24 +0300

    See All Featured Recent Popular Missing

---
+++ lib/ui/components/search_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return buildSearchBar();
+  }
+}
+
+@override
+Widget buildSearchBar() => const TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+        hintText: 'Content, people, topic, collection, or ask',
+        hintStyle: TextStyle(
+            fontFamily: "SFCompactText-Regular",
+            color: Color(0xff949494),
+            fontWeight: FontWeight.w400,
+            fontSize: 13),
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+    );
--- lib/ui/screens/horizontal_contentList_see_all/see_all_featured_collections.dart
+++ lib/ui/screens/horizontal_contentList_see_all/see_all_featured_collections.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:upcarta_mobile_app/ui/components/collection_card.dart';
+import 'package:upcarta_mobile_app/ui/components/search_bar.dart';
+
+import '../../../util/constants.dart';
 
 class SeeAll_FeaturedCollections extends StatefulWidget {
   const SeeAll_FeaturedCollections({Key? key}) : super(key: key);
@@ -12,6 +16,50 @@ class _SeeAll_FeaturedCollectionsState
     extends State<SeeAll_FeaturedCollections> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Color(0xFFC4C4C4),
+            ),
+            onPressed: () => Navigator.of(context).pop()),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        titleSpacing: 0.0,
+        title: const Text(
+          'Featured Collections',
+          style: TextStyle(
+              fontFamily: "SFCompactText-Medium.ttf",
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 20),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(children: [
+          SizedBox(height: 32, child: SearchBar()),
+          SizedBox(height: 12),
+          SingleChildScrollView(
+            child: Container(
+              height: 600,
+              child: Expanded(
+                child: ListView.builder(
+                  // shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.only(top: 16),
+                  itemCount: contents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CollectionCard(content: contents[index]);
+                  },
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
