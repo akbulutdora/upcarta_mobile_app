63c95fc30a46d4a7d113d05ba273a627ec15fc33
akbulutdora <akbulutdora@gmail.com>
2022-05-18 21:21:06 +0300

    commit for can

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:upcarta_mobile_app/blocs/app_bloc/bloc/app_bloc.dart';
 import 'firebase_options.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:bloc/bloc.dart';
 import 'package:upcarta_mobile_app/navigation//routes.gr.dart';
+import 'package:upcarta_mobile_app/repositories/auth_repository.dart';
+import 'app/app.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
-void main() async {
+Future<void> main() async {
   return BlocOverrides.runZoned(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
       );
-
-      /* TODO: INJECT AUTHENTICATION REPOSITORY
-      final authenticationRepository = AuthenticationRepository();
-      await authenticationRepository.user.first;
-      runApp(App(authenticationRepository: authenticationRepository)); */
-
-      runApp(MyFirebaseApp());
+      final authRepository = AuthRepository();
+      // await authRepository.user.first;
+      runApp(App(
+        authRepository: authRepository,
+      ));
     },
-    // blocObserver: AppBlocObserver(),
+    blocObserver: AppBlocObserver(),
   );
+}
+
+class Welcome extends StatefulWidget {
+  Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Upcarta',
+      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
+      theme: ThemeData.light(),
+      builder: (context, router) => router!,
+    );
+  }
 }
 
 class MyFirebaseApp extends StatelessWidget {
   final Future<FirebaseApp> _init = Firebase.initializeApp();
-  final _appRouter = AppRouter();
-
-  MyFirebaseApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -44,22 +61,16 @@ class MyFirebaseApp extends StatelessWidget {
         if (snapshot.hasError) {
           return const Scaffold(
             body: Center(
-              child: Text("Error: cannot initialize firebase"),
+              child: Text("Error"),
             ),
           );
         }
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp.router(
-            title: 'Upcarta',
-            routeInformationParser: _appRouter.defaultRouteParser(),
-            routerDelegate: _appRouter.delegate(),
-            theme: ThemeData.light(),
-            builder: (context, router) => router!,
-          );
+          return Welcome();
         }
         return const MaterialApp(
           home: Center(
-            child: Text("Waiting to initialize firebase"),
+            child: Text("Waiting"),
           ),
         );
       },
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,67 +4,80 @@ import 'package:json_annotation/json_annotation.dart';
 part "user.g.dart";
 
 @JsonSerializable()
-class User extends Equatable{
+class User extends Equatable {
   final String id;
   final String name;
   final String username;
   final String email;
   final int followers;
   final int following;
-  final List<int> followerIDs;
-  final List<int> followingIDs;
+  final List<int>? followerIDs;
+  final List<int>? followingIDs;
   final String avatar;
   final String bio;
   final int recommendationCount;
   final String recommendationsID;
   final String savesID;
-  final List<int> collectionsIDs;
-  final List<int> asksIDs;
-  final DateTime joinDate;
+  final List<int>? collectionsIDs;
+  final List<int>? asksIDs;
+  final DateTime? joinDate;
 
-  User({
+  const User({
     required this.username,
     required this.id,
     required this.name,
     required this.email,
     required this.followers,
     required this.following,
-    required this.joinDate,
+    this.joinDate,
     required this.avatar,
     required this.bio,
-    required this.followerIDs,
-    required this.followingIDs,
+    this.followerIDs,
+    this.followingIDs,
     required this.recommendationCount,
     required this.recommendationsID,
     required this.savesID,
-    required this.collectionsIDs,
-    required this.asksIDs,
+    this.collectionsIDs,
+    this.asksIDs,
   });
 
-  User.empty(
-      {this.id = "",
-      this.name = "",
-      this.username = "",
-      this.email = "",
-      this.followers = 0,
-      this.following = 0,
-      required this.followerIDs,
-      required this.followingIDs,
-      this.avatar = "",
-      this.bio = "",
-      this.recommendationCount = 0,
-      this.recommendationsID = "",
-      this.savesID = "",
-      required this.collectionsIDs,
-      required this.asksIDs,
-      required this.joinDate});
+  static const empty = User(
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    followers: 0,
+    following: 0,
+    avatar: "",
+    bio: "",
+    recommendationCount: 0,
+    recommendationsID: "",
+    savesID: "",
+  );
+
+  bool get isEmpty => this == User.empty;
+  bool get isNotEmpty => this != User.empty;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
 
   @override
-  List<Object?> get props => [id, name, username, email, followers,following,followerIDs,followingIDs,
-                              avatar,bio,recommendationCount,recommendationsID,savesID,collectionsIDs,
-                              asksIDs,joinDate
-                             ];
+  List<Object?> get props => [
+        id,
+        name,
+        username,
+        email,
+        followers,
+        following,
+        followerIDs,
+        followingIDs,
+        avatar,
+        bio,
+        recommendationCount,
+        recommendationsID,
+        savesID,
+        collectionsIDs,
+        asksIDs,
+        joinDate
+      ];
 }
