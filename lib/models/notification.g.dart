6196feb4bb78f0a1de4fd314deb0c60a527c060d
Can <thcnkrmn@outlook.com>
2022-05-21 23:23:12 +0300

    main reverting

---
+++ lib/models/notification.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notification.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Notification _$NotificationFromJson(Map<String, dynamic> json) => Notification(
+      text: json['text'] as String,
+      image: json['image'] as String,
+      username: json['username'] as String,
+      contentID: json['contentID'] as String,
+      date: DateTime.parse(json['date'] as String),
+    );
+
+Map<String, dynamic> _$NotificationToJson(Notification instance) =>
+    <String, dynamic>{
+      'text': instance.text,
+      'image': instance.image,
+      'username': instance.username,
+      'contentID': instance.contentID,
+      'date': instance.date.toIso8601String(),
+    };
