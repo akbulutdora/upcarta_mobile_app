4ad825e7501c14a041a25dbbdff9ec8a0fab308f
akbulutdora <akbulutdora@gmail.com>
2022-05-22 18:25:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,56 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  //runApp(Welcome());
+  runApp(MyFirebaseApp());
 }
+
 class Welcome extends StatefulWidget {
   Welcome({Key? key}) : super(key: key);
+
   @override
   State<Welcome> createState() => _WelcomeState();
-  static MaterialPage page() {
-    return MaterialPage(
-      child: Welcome(),
+}
+
+class _WelcomeState extends State<Welcome> {
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Upcarta',
+      routeInformationParser: _appRouter.defaultRouteParser(),
+      routerDelegate: _appRouter.delegate(),
+      theme: ThemeData.light(),
+      builder: (context, router) => router!,
     );
   }
 }
-// class MyFirebaseApp extends StatelessWidget {
-//   final Future<FirebaseApp> _init = Firebase.initializeApp();
-@override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       future: _init,
-//       builder: (context, snapshot) {
-//         if (snapshot.hasError) {
-//           return const Scaffold(
-//             body: Center(
-//               child: Text("Error"),
-//             ),
-//           );
-//         }
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return Welcome();
-//         }
-//         return const MaterialApp(
-//           home: Center(
-//             child: Text("Waiting"),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
+
+class MyFirebaseApp extends StatelessWidget {
+  final Future<FirebaseApp> _init = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _init,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text("Error"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Welcome();
+        }
+        return const MaterialApp(
+          home: Center(
+            child: Text("Waiting"),
+          ),
+        );
+      },
+    );
+  }
+}
