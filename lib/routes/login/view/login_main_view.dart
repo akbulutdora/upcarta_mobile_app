f70f8b84565cb33173da74524b157aeb43398b31
Can <thcnkrmn@outlook.com>
2022-05-25 21:34:52 +0300

    google sign in

--- lib/routes/login/view/login_main_view.dart
+++ lib/routes/login/view/login_main_view.dart
@@ -7,6 +7,9 @@ import 'package:upcarta_mobile_app/util/styles.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:upcarta_mobile_app/navigation/routes.gr.dart';
 
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreen();
@@ -26,6 +29,26 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreen extends State<LoginScreen> {
   // final _formKey = GlobalKey<FormState>();
+
+  Future<void> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    print("SIGNED IN");
+    print(FirebaseAuth.instance.signInWithCredential(credential));
+
+    // // Once signed in, return the UserCredential
+    // return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,192 +85,175 @@ class _LoginScreen extends State<LoginScreen> {
         backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Stack(
+          child: Column(
             children: [
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(
-                        top: height * 0.05,
-                      ),
-                      child: Text(
-                        'Discover, collect \n and share great content.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black, fontSize: height / 30),
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: height * 0.05,
                     ),
+                    child: Text(
+                      'Discover, collect \n and share great content.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.black, fontSize: height / 30),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: height * .10),
+              SizedBox(
+                //height: height * 0.07,
+                width: width,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: Colors.lightBlue,
+                      padding: EdgeInsets.symmetric(vertical: height * 0.03)),
+                  child: Text(
+                    'Continue with Twitter',
+                    style:
+                        TextStyle(color: Colors.white, fontSize: height / 50),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * .025),
+              SizedBox(
+                //height: height * 0.07,
+                width: width,
+                child: OutlinedButton(
+                  onPressed: () {
+                    signInWithGoogle();
+                  },
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(vertical: height * 0.03)),
+                  child: Text(
+                    'Continue with Google',
+                    style:
+                        TextStyle(color: Colors.black, fontSize: height / 50),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * .025),
+              SizedBox(
+                //height: height * 0.07,
+                width: width,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: Colors.black,
+                      padding: EdgeInsets.symmetric(vertical: height * 0.03)),
+                  child: Text(
+                    'Continue with Apple',
+                    style:
+                        TextStyle(color: Colors.white, fontSize: height / 50),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * .03),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Divider(
+                          thickness: height * 0.001,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Text("     OR     "),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Divider(
+                          thickness: height * 0.001,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: height * .03),
+              SizedBox(
+                //height: height * 0.07,
+                width: width * 1,
+                child: OutlinedButton(
+                  onPressed: () async {
+                    context.router.push(const MyRegisterRoute());
+                  },
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: Colors.indigoAccent,
+                      padding: EdgeInsets.symmetric(vertical: height * 0.03)),
+                  child: Text(
+                    'Sign Up',
+                    style:
+                        TextStyle(color: Colors.white, fontSize: height / 50),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * .015),
+              SizedBox(
+                width: width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: height * 0.015,
+                          color: Colors.black,
+                        ),
+                        children: const <TextSpan>[
+                          TextSpan(text: 'By signing up you agree to our '),
+                          TextSpan(
+                              text: '\nTerms, Privacy Policy, and Cookie Use',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-                SizedBox(height: height * .10),
-                SizedBox(
-                  //height: height * 0.07,
-                  width: width,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: Colors.lightBlue,
-                        padding: EdgeInsets.symmetric(vertical: height * 0.03)),
-                    child: Text(
-                      'Continue with Twitter',
-                      style:
-                          TextStyle(color: Colors.white, fontSize: height / 50),
-                    ),
+              ),
+              //SizedBox(height: height * .2),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Have an account already?',
+                    style: TextStyle(fontSize: height * 0.02),
                   ),
-                ),
-                SizedBox(height: height * .025),
-                SizedBox(
-                  //height: height * 0.07,
-                  width: width,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: Colors.white,
-                        padding: EdgeInsets.symmetric(vertical: height * 0.03)),
-                    child: Text(
-                      'Continue with Google',
-                      style:
-                          TextStyle(color: Colors.black, fontSize: height / 50),
-                    ),
-                  ),
-                ),
-                SizedBox(height: height * .025),
-                SizedBox(
-                  //height: height * 0.07,
-                  width: width,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: Colors.black,
-                        padding: EdgeInsets.symmetric(vertical: height * 0.03)),
-                    child: Text(
-                      'Continue with Apple',
-                      style:
-                          TextStyle(color: Colors.white, fontSize: height / 50),
-                    ),
-                  ),
-                ),
-              ]),
-              SizedBox(height: height * .025),
-              SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.5,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(height: height * .03),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Divider(
-                                  thickness: height * 0.001,
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                          ),
-                          const Text("     OR     "),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Divider(
-                                  thickness: height * 0.001,
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Colors.lightBlue,
                       ),
-                      SizedBox(height: height * .01),
-                      SizedBox(
-                        //height: height * 0.07,
-                        width: width * 1,
-                        child: OutlinedButton(
-                          onPressed: () async {
-                            context.router.push(const MyRegisterRoute());
-                          },
-                          style: OutlinedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(12)),
-                              backgroundColor: Colors.indigoAccent,
-                              padding: EdgeInsets.symmetric(
-                                  vertical: height * 0.03)),
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(
-                                color: Colors.white, fontSize: height / 50),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: height * .015),
-                      SizedBox(
-                        width: width,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: height * 0.015,
-                                  color: Colors.black,
-                                ),
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                      text: 'By signing up you agree to our '),
-                                  TextSpan(
-                                      text:
-                                          '\nTerms, Privacy Policy, and Cookie Use',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      //SizedBox(height: height * .2),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Have an account already?',
-                            style: TextStyle(fontSize: height * 0.02),
-                          ),
-                          TextButton(
-                              style: TextButton.styleFrom(
-                                primary: Colors.lightBlue,
-                              ),
-                              onPressed: () async {
-                                context.router.push(const LoginScreen2Route());
-                              },
-                              child: const Text("LOG IN")),
-                        ],
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          SharedPreferences prefs =
-                              await SharedPreferences.getInstance();
-                          prefs.setBool("landed", false);
-                        },
-                        child: const Text("Press to cancel landed"),
-                      ),
-                    ],
-                  ),
-                ),
+                      onPressed: () async {
+                        context.router.push(const LoginScreen2Route());
+                      },
+                      child: const Text("LOG IN")),
+                ],
+              ),
+              TextButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setBool("landed", false);
+                },
+                child: const Text("Press to cancel landed"),
               ),
             ],
           ),
