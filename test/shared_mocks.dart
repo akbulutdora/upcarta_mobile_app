f35916629769b7e17270d13fce12fc8bdab7662a
akbulutdora <akbulutdora@gmail.com>
2022-07-15 13:21:15 +0300

    update shared_mocks.dart

--- test/shared_mocks.dart
+++ test/shared_mocks.dart
@@ -4,9 +4,11 @@ import 'package:upcarta_mobile_app/core/api/data_sources/local_data_storage.dart
 import 'package:upcarta_mobile_app/core/api/data_sources/remote_data_source.dart';
 import 'package:upcarta_mobile_app/core/platform/network_info.dart';
 
-@GenerateMocks([NetworkInfo, RemoteDataSource, LocalDataStorage, SharedPreferences])
+@GenerateMocks(
+    [NetworkInfo, RemoteDataSource, LocalDataStorage, SharedPreferences])
 import 'shared_mocks.mocks.dart';
 
 void main() {
-
+  MockRemoteDataSource mockRemoteDataSource = MockRemoteDataSource();
+  mockRemoteDataSource.toString();
 }
